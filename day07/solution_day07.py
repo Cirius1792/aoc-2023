faabe3e597174ded0525355d432d3ac0c398f7f2
Ciro Lucio Tecce <cirolucio.tecce@gmail.com>
2023-12-10 12:19:57 +0100

    Refactoring for problem 7 part 2

--- day07/solution_day07.py
+++ day07/solution_day07.py
@@ -1,6 +1,6 @@
 from typing import List, Generator
 from collections import namedtuple, Counter
-from functools import cmp_to_key 
+from functools import cmp_to_key
 
 Hand = namedtuple("Hand", "cards bid")
 
@@ -15,30 +15,49 @@ def parse_input(input_lines: List[str] | Generator) -> List[Hand]:
 
 
 ALPHABET = "AKQJT98765432"
-def compare_hands(hand_1: Hand, hand_2: Hand) -> int:
+
+
+def compare_hand_points(hand_1: Hand, hand_2: Hand):
     c_1 = Counter(hand_1.cards)
     c_2 = Counter(hand_2.cards)
     mc_1 = c_1.most_common()
     mc_2 = c_2.most_common()
 
-#    print("-"*10)
-#    print(f"Comparing Cards: {hand_1.cards} vs {hand_2.cards}")
+    #    print("-"*10)
+    #    print(f"Comparing Cards: {hand_1.cards} vs {hand_2.cards}")
     for i in range(min(len(mc_1), len(mc_2))):
-        #print(f"Comparing:  {mc_1[i]} vs {mc_2[i]}")
+        # print(f"Comparing:  {mc_1[i]} vs {mc_2[i]}")
         if mc_1[i][1] > mc_2[i][1]:
-            #print("Hand 2 wins")
+            # print("Hand 2 wins")
             return 1
         if mc_1[i][1] < mc_2[i][1]:
-            #print("Hand 1 wins")
+            # print("Hand 1 wins")
             return -1
-    #print("Same points. Comparing the first different highest card")
+    return 0
+
+
+def compare_hands(
+    hand_1: Hand,
+    hand_2: Hand,
+    card_points_comparator=compare_hand_points,
+    card_priorities=ALPHABET,
+) -> int:
+    card_points = card_points_comparator(hand_1, hand_2)
+    if card_points != 0:
+        return card_points
+
+    # print("Same points. Comparing the first different highest card")
     for i in range(len(hand_1.cards)):
-        #print(f"Comparing:  {hand_1.cards[i]} vs {hand_2.cards[i]}")
-        if ALPHABET.index(hand_1.cards[i]) > ALPHABET.index(hand_2.cards[i]):
-            #print("Hand 1 wins")
+        # print(f"Comparing:  {hand_1.cards[i]} vs {hand_2.cards[i]}")
+        if card_priorities.index(hand_1.cards[i]) > card_priorities.index(
+            hand_2.cards[i]
+        ):
+            # print("Hand 1 wins")
             return -1
-        if ALPHABET.index(hand_1.cards[i]) < ALPHABET.index(hand_2.cards[i]):
-            #print("Hand 2 wins")
+        if card_priorities.index(hand_1.cards[i]) < card_priorities.index(
+            hand_2.cards[i]
+        ):
+            # print("Hand 2 wins")
             return 1
     return 0
 
@@ -46,10 +65,12 @@ def compare_hands(hand_1: Hand, hand_2: Hand) -> int:
 def rank_hands(input_hands: List[Hand]) -> List[Hand]:
     return sorted(input_hands, key=cmp_to_key(compare_hands))
 
-def solve(input_lines:List[str]) -> int:
+
+def solve(input_lines: List[str]) -> int:
     hands = parse_input(input_lines)
     ranked_hands = rank_hands(hands)
-    return sum([(i+1)*hand.bid for i, hand in enumerate(ranked_hands)])
+    return sum([(i + 1) * hand.bid for i, hand in enumerate(ranked_hands)])
+
 
 if __name__ == "__main__":
     with open("./day07/input.txt", "r") as reader:
