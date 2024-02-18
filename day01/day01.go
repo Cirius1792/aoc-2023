c6df37cecd73303ee10d74b4a4fe8014449eb143
cirius1792 <cirolucio.tecce@gmail.com>
2024-02-18 11:40:45 +0100

    day01 part 2

--- day01/day01.go
+++ day01/day01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // Given a string of digits and characters, returns the first and the last digits in the string
@@ -43,22 +44,81 @@ func Solve(line string) int {
 	return converted_digit
 }
 
+// Given a string of digits and characters, returns the first and the last digits in the string
+// both numbers and words are considered when looking for digits
+// Parameters:
+//   - line: string of digits and characters
+//
+// Returns:
+//   - int: first and last digits in the string
+//
+// Examples:
+//   - "1abc2" => 12
+//   - "a1b2c3" => 13
+//   - "one2asd8" => 18
+func SolvePart2(line string) int {
+
+	WORD_TO_DIGIT := map[string]string{
+		"1":     "1",
+		"2":     "2",
+		"3":     "3",
+		"4":     "4",
+		"5":     "5",
+		"6":     "6",
+		"7":     "7",
+		"8":     "8",
+		"9":     "9",
+		"0":     "0",
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	var first_digit string
+	digit_index := len(line) + 1
+	for word, digit := range WORD_TO_DIGIT {
+		if occurrence_index := strings.Index(line, word); occurrence_index > -1 && occurrence_index < digit_index {
+			digit_index = occurrence_index
+			first_digit = digit
+		}
+	}
+	var second_digit string
+	digit_index = -1
+	for word, digit := range WORD_TO_DIGIT {
+		if occurrence_index := strings.LastIndex(line, word); occurrence_index > -1 && occurrence_index > digit_index {
+			digit_index = occurrence_index
+			second_digit = digit
+		}
+	}
+	converted_digit, _ := strconv.Atoi(first_digit + second_digit)
+	return converted_digit
+
+}
+
 func main() {
-	fmt.Println("Part 1:")
-    solution := 0
+	solutionPart1 := 0
+	solutionPart2 := 0
 	// Read from input.txt
 	file, err := os.OpenFile("input.txt", os.O_RDONLY, 0)
-    if err != nil {
-        fmt.Println("Failed to read input file")
-        return
-    }
-    defer file.Close()
+	if err != nil {
+		fmt.Println("Failed to read input file")
+		return
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        line_sol := Solve(line)
-        solution = solution + line_sol 
-    }
-    fmt.Println("Solution: ", solution)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_sol := Solve(line)
+		solutionPart1 = solutionPart1 + line_sol
+		line_sol = SolvePart2(line)
+		solutionPart2 = solutionPart2 + line_sol
+	}
+	fmt.Println("Solution Part 1: ", solutionPart1)
+	fmt.Println("Solution Part 2: ", solutionPart2)
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -3,40 +3,75 @@ package main
 import "testing"
 
 func TestShouldReturnFirstAndLastDigit(t *testing.T) {
-    text := "1abc2"
-    expected := 12
-    actual := Solve(text)
-    if actual != expected {
-        t.Errorf("Expected %d, got %d", expected, actual)
-    }
+	text := "1abc2"
+	expected := 12
+	actual := Solve(text)
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
 
-
-func Test_should_return_the_digits_in_the_middle(t *testing.T){
-    line := "pqr3stu8vwx"
-    actual := Solve(line)
-    expected := 38
-    if actual != expected {
-        t.Errorf("Expected %d, got %d", expected, actual)
-    }
+func Test_should_return_the_digits_in_the_middle(t *testing.T) {
+	line := "pqr3stu8vwx"
+	actual := Solve(line)
+	expected := 38
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
 
-func Test_should_take_only_the_first_digits_found(t *testing.T){
-    line := "1b2c3d4e5f"
-    actual := Solve(line)
-    expected := 15
-    if actual != expected {
-        t.Errorf("Expected %d, got %d", expected, actual)
-    }
+func Test_should_take_only_the_first_digits_found(t *testing.T) {
+	line := "1b2c3d4e5f"
+	actual := Solve(line)
+	expected := 15
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
 
-func Test_should_take_the_only_digit_as_first_and_last(t *testing.T){
-    line := "treb7uchet"
-    actual := Solve(line)
-    expected :=77
-    if actual != expected {
-        t.Errorf("Expected %d, got %d", expected, actual)
-    }
+func Test_should_take_the_only_digit_as_first_and_last(t *testing.T) {
+	line := "treb7uchet"
+	actual := Solve(line)
+	expected := 77
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 
+}
+
+func TestShouldReturnFirstAndLastDigit_P2(t *testing.T) {
+	text := "1abc2"
+	expected := 12
+	actual := SolvePart2(text)
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
+}
+
+func Test_should_return_the_digits_in_the_middle_P2(t *testing.T) {
+	line := "pqr3stu8vwx"
+	actual := SolvePart2(line)
+	expected := 38
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
+}
+
+func Test_should_take_only_the_first_digits_found_P2(t *testing.T) {
+	line := "one1b2c3d4e5f"
+	actual := SolvePart2(line)
+	expected := 15
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
+}
+
+func Test_should_take_the_only_digit_as_first_and_last_P2(t *testing.T) {
+	line := "treb7uchet"
+	actual := SolvePart2(line)
+	expected := 77
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 
 }
